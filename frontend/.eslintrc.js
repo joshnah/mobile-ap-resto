2b73ef8cfb7e501d8e924d60e8e28b5f8ce4f6b0
joshnah <tiendatamser296@gmail.com>
2023-04-27 23:48:06 +0200

    ci-cd lint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:cypress/recommended"
     ],
     "overrides": [
     ],
@@ -17,5 +19,17 @@ module.exports = {
         "react"
     ],
     "rules": {
+    },
+    "settings": {
+        "react": {
+          "createClass": "createReactClass", // Regex for Component Factory to use,
+                                             // default to "createReactClass"
+          "pragma": "React",  // Pragma to use, default to "React"
+          "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+          "version": "detect", // React version. "detect" automatically picks the version you have installed.
+                               // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                               // It will default to "latest" and warn if missing, and to "detect" in the future
+          "flowVersion": "0.53" // Flow version
+        }
     }
 }
