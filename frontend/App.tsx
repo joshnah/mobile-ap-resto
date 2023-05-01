86e98610116dd3821f41791f156e8d140f82c0ce
joshnah <tiendatamser296@gmail.com>
2023-05-01 19:33:59 +0200

    add store  to front + api user returns user data

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Box, NativeBaseProvider, Text } from "native-base";
 import { login } from "./src/services/dataService";
 import { store } from "./src/store/store";
+import LoginPage from "./src/components/login";
 
 export default function App() {
   login("admin@gmail.com", "admin");
@@ -15,6 +16,8 @@ export default function App() {
       <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
         <Text>Open up App.js to start working on your app!</Text>
       </Box>
+
+      <LoginPage></LoginPage>
     </NativeBaseProvider>
   );
 }
--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,4 +1,9 @@
+import { store } from "../store/store";
+
 export default function LoginPage() {
+    // get list of resaurants from store
+    // store.subscribe(() => console.log(store.getState()));
+
     return (
         <div>
             <h1>Login</h1>
