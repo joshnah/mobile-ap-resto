4e7878b4f490191d2624ce0e733f21795a6ff13d
joshnah <tiendatamser296@gmail.com>
2023-05-29 20:04:22 +0200

    apk generation

---
+++ genereapk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+mkdir -p public # Crée le répertoire pour disposer dans pages de l'application 
+source /etc/profile.d/androidrc.sh # Définit les variables d'environnement pour accéder aux outils nécessaires
+npm install
+npx expo prebuild  # Prépare la génération de l'application native
+cd android    # Se place dans le répertoire android
+./gradlew assembleRelease # Puis génère l'application au format apk 
+cp ./app/build/outputs/apk/release/app-release.apk ../public/monapp.apk # Déplace l'application générée dans /public 
+exit 0
