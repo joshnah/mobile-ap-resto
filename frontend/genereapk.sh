e520873073c2a5ef6cdf291a432b60e55074337f
joshnah <tiendatamser296@gmail.com>
2023-05-29 22:08:02 +0200

    fix cicd

--- frontend/genereapk.sh
+++ frontend/genereapk.sh
@@ -2,7 +2,7 @@
 mkdir -p public # Crée le répertoire pour disposer dans pages de l'application 
 source /etc/profile.d/androidrc.sh # Définit les variables d'environnement pour accéder aux outils nécessaires
 npm install
-npx install expo
+npm install expo
 npx expo prebuild  # Prépare la génération de l'application native
 cd android    # Se place dans le répertoire android
 ./gradlew assembleRelease # Puis génère l'application au format apk 
